c0104082c158d3e03adee3b69158f3e3658105d1
Manlove <Manlove.Logan@mayo.edu>
2019-02-11 11:58:40 -0600

    Commenting

--- folder_fix.py
+++ folder_fix.py
@@ -2,17 +2,22 @@ from os import chdir, rename, removedirs, listdir
 from os.path import join, exists, isdir
 from shutil import rmtree
 
-basepath = "C:\\Users\\M113455\\Desktop\\empty"
+basepath = "X:\\LABORATORY\\JCMS\\"
 
+# Runs until all folders within the basepath directory have been removed
 while listdir(basepath) != []:
     path = basepath
     j=0
     while True:
+        # checks to see if a folder exists with the current integer name and increments by one if it does
         if exists(join(basepath,str(j))):
             j += 1
         else:
+        # Steps through
             for i in range(1,10):
                 dr = listdir(path)
+                # !!! This will probably break if there are more than 1 directory in the current folder
+                # adds the two folders in series instead of working through them in parallel
                 for i in dr:
                     if isdir(join(path,i)):
                         path = join(path,i)
@@ -28,12 +33,3 @@ while listdir(basepath) != []:
             rmtree(join(basepath,i))
         except:
             print("skipped {}".format(i))
-    
-        
-
-
-
-
-
-    
-    
